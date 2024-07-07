98b8ba929bc26bca4e6390db77f8212e405ea066
Bal67 <bal67@duke.edu>
2024-07-06 21:56:44 -0500

    fix

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -1,1 +1,17 @@
+import re
+import numpy as np
+import pandas as pd
 
+import nltk
+
+# plotting
+import seaborn as sns
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+# sklearn
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import confusion_matrix, classification_report
