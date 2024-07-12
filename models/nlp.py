e789c8734811dafbc0a4ff4e089553c5a357033d
Bal67 <bal67@duke.edu>
2024-07-12 00:43:23 -0500

    commit

--- models/nlp.py
+++ models/nlp.py
@@ -8,14 +8,22 @@ from tensorflow.keras.layers import Embedding, Dense, SpatialDropout1D, LSTM
 from tensorflow.keras.optimizers import Adam
 from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import Tokenizer
+from tensorflow.keras.callbacks import EarlyStopping
 import pickle
+import os
+import logging
+import tensorflow as tf
+
+# Suppress TensorFlow warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+logging.getLogger('tensorflow').setLevel(logging.FATAL)
 
 sys.path.insert(0, '/content/drive/MyDrive/TextSentiment/NLP_Project_540/scripts/dataset.py')
 MODEL_FILE = "/content/drive/MyDrive/TextSentiment/NLP_Project_540/models/nlp_model.h5"
 
 def train_nlp_model():
     # Load and preprocess dataset
-    path = '/content/drive/MyDrive/TextSentiment/NLP_Project_540/data/preprocessed_dataset.csv'  # Update with the correct path
+    path = '/content/drive/MyDrive/TextSentiment/NLP_Project_540/data/preprocessed_dataset.csv'
     df = pd.read_csv(path)
 
     # Ensure all entries in 'cleaned_tweet' are strings and handle missing values
@@ -40,23 +48,30 @@ def train_nlp_model():
     
     model.compile(loss='binary_crossentropy', optimizer=Adam(), metrics=['accuracy'])
     
-    model.fit(X_train, y_train, epochs=5, batch_size=64, validation_data=(X_val, y_val), verbose=2)
+    # Implement EarlyStopping
+    early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
     
+    # Profile the training process
+    tf.profiler.experimental.start('/content/logs')
+    
+    model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_val, y_val), verbose=2, callbacks=[early_stopping])
+    
+    tf.profiler.experimental.stop()
+
     # Save the tokenizer to Google Drive
     tokenizer_save_path = '/content/drive/MyDrive/TextSentiment/NLP_Project_540/models/nlp_tokenizer.pkl'
     with open(tokenizer_save_path, 'wb') as handle:
-        pickle.dump(tokenizer, handle, protocol= pickle.HIGHEST_PROTOCOL)
+        pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     # Evaluate the model on the test set
     y_pred = (model.predict(X_test) > 0.5).astype("int32")
     print(f'NLP Model Accuracy: {accuracy_score(y_test, y_pred)}')
     print(classification_report(y_test, y_pred))
 
-    #Save the model to Google Drive
+    # Save the model to Google Drive
     model.save(MODEL_FILE)
         
     return model, tokenizer
 
 if __name__ == "__main__":
     train_nlp_model()
-
